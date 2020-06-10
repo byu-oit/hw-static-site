a067c05a3ef70bcbd6660aec21c051bd98ddbf3d
Josh Gubler <josh.gubler@gmail.com>
2020-06-10 15:20:01 -0600

    Initial commit

---
+++ terraform-iac/prd/app/main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_version = "0.12.26"
+  backend "s3" {
+    bucket         = "terraform-state-storage-<account_number>"
+    dynamodb_table = "terraform-state-lock-<account_number>"
+    key            = "hello-world-api-prd/app.tfstate"
+    region         = "us-west-2"
+  }
+}
+
+provider "aws" {
+  version = "~> 2.42"
+  region  = "us-west-2"
+}
+
+variable "image_tag" {
+  type = string
+}
+
+module "app" {
+  source                           = "../../modules/app/"
+  env                              = "prd"
+  image_tag                        = var.image_tag
+  codedeploy_termination_wait_time = 15
+}
+
+output "url" {
+  value = module.app.url
+}
+
+output "codedeploy_app_name" {
+  value = module.app.codedeploy_app_name
+}
+
+output "codedeploy_deployment_group_name" {
+  value = module.app.codedeploy_deployment_group_name
+}
+
+output "codedeploy_appspec_json_file" {
+  value = module.app.codedeploy_appspec_json_file
+}
