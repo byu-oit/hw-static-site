20df0108fe9df459be65a32dcf1f4a0f8e0e6be4
Blake Morgan <blake_morgan@byu.edu>
2021-06-10 10:41:33 -0600

    Shorten bucket name

--- terraform-iac/modules/app/main.tf
+++ terraform-iac/modules/app/main.tf
@@ -3,10 +3,11 @@ variable "env" {
 }
 
 locals {
+  app_name = "hw-static-site"
   # These three lines form the URL for your website.
   # In real life, you should probably use a more human-friendly URL.
   # Something like, "mysite.byu.edu" for prd and "mysite-dev.byu.edu" for dev.
-  subdomain = (var.env == "prd") ? "hw-static-site" : "hw-static-site-${var.env}"
+  subdomain = (var.env == "prd") ? local.app_name : "${local.app_name}-${var.env}"
   parent    = (var.env == "prd" || var.env == "cpy") ? "byu-oit-terraform-prd.amazon.byu.edu" : "byu-oit-terraform-dev.amazon.byu.edu"
   url       = "${local.subdomain}.${local.parent}"
 }
@@ -19,7 +20,7 @@ module "s3_site" {
   source         = "github.com/byu-oit/terraform-aws-s3staticsite?ref=v5.1.1"
   site_url       = local.url
   hosted_zone_id = data.aws_route53_zone.zone.id
-  s3_bucket_name = local.url
+  s3_bucket_name = "${local.app_name}-${var.env}"
 }
 
 output "s3_bucket" {
