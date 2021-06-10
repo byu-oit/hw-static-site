b683d2afb6389264d988c04471801e1b98f22b7a
Blake Morgan <blake_morgan@byu.edu>
2021-06-10 12:31:29 -0600

    Update workflows and Terraform (#34) (#35)
    * Update workflows and Terraform
    
    * Restore terraform init
    
    * Format terraform
    
    * Update .github/dependabot.yml
    
    Co-authored-by: Scott Hutchings <yoshutch@users.noreply.github.com>
    
    * Update terraform-iac/prd/setup/setup.tf
    
    Co-authored-by: Scott Hutchings <yoshutch@users.noreply.github.com>
    
    * Add dev/stg lock files
    
    * Add dev/stg lock files
    
    * Add prd/cpy terraform lock files
    
    * Update default tags block
    
    * Format terraform
    
    * Shorten bucket name
    
    * Update s3 static site
    
    Co-authored-by: Scott Hutchings <yoshutch@users.noreply.github.com>
    
    Co-authored-by: Scott Hutchings <yoshutch@users.noreply.github.com>

--- terraform-iac/cpy/setup/setup.tf
+++ terraform-iac/cpy/setup/setup.tf
@@ -1,20 +1,40 @@
 terraform {
+  required_version = "1.0.0"
+
   backend "s3" {
     bucket         = "terraform-state-storage-539738229445"
     dynamodb_table = "terraform-state-lock-539738229445"
     key            = "hw-static-site-cpy/setup.tfstate"
     region         = "us-west-2"
   }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+locals {
+  env = "cpy"
 }
 
 provider "aws" {
-  version = "~> 2.42"
-  region  = "us-west-2"
+  region = "us-west-2"
+
+  default_tags {
+    tags = {
+      env              = local.env
+      data-sensitivity = "public"
+      repo             = "https://github.com/byu-oit/hw-static-site"
+    }
+  }
 }
 
 module "setup" {
   source = "../../modules/setup/"
-  env    = "cpy"
+  env    = local.env
 }
 
 output "hosted_zone_id" {
