67f8d06cdd7b609a305f495553fb954f4870a4ea
Josh Gubler <josh.gubler@gmail.com>
2023-04-21 08:41:37 -0600

    bump terraform aws provider

--- terraform-iac/prd/app/prd-app.tf
+++ terraform-iac/prd/app/prd-app.tf
@@ -11,7 +11,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.63"
+      version = "~> 4.64"
     }
     random = {
       source  = "hashicorp/random"
